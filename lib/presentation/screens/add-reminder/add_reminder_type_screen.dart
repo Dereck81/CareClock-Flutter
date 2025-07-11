206c38ac96bee6277231f847f77859a51a5f8825
Dereck81 <derecknova23@gmail.com>
2025-07-11 18:27:24 -0500

    formatted code

--- lib/presentation/screens/add-reminder/add_reminder_type_screen.dart
+++ lib/presentation/screens/add-reminder/add_reminder_type_screen.dart
@@ -8,51 +8,56 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 class AddReminderTypeScreen extends StatelessWidget {
   const AddReminderTypeScreen({super.key});
 
-  Widget _button(String imagePath, String title, String content, void Function() onPress) {
+  Widget _button(
+    String imagePath,
+    String title,
+    String content,
+    void Function() onPress,
+  ) {
     return CustomButton(
-			onPressed: onPress,
-			child: CustomContainer(
-				paddingX: 15.w,
-				paddingY: 10.h,
-				marginX: 15.w,
-				child: Row(
-					crossAxisAlignment: CrossAxisAlignment.start,
-					spacing: 10.w,
-					children: <Widget>[
-						ClipRRect(
-							borderRadius: BorderRadiusGeometry.circular(35.r),
-							child: Image.asset(
-								imagePath,
-								height: 100.h,
-								width: 130.w,
-								fit: BoxFit.cover,
-							),
-						),
-						Expanded(
-							child: Column(
-								crossAxisAlignment: CrossAxisAlignment.start,
-								children: <Widget>[
-									Text(
-										title,
-										style: TextStyle(
-											color: AppColors.textColor.value,
-											fontSize: FontScaler.fromSize(FontSize.xl),
-										),
-									),
-									Text(
-										content,
-										style: TextStyle(
-											color: AppColors.textColor.value,
-											fontSize: FontScaler.fromSize(FontSize.lg),
-										),
-									),
-								],
-							),
-						),
-					],
-				),
-			),
-		);
+      onPressed: onPress,
+      child: CustomContainer(
+        paddingX: 15.w,
+        paddingY: 10.h,
+        marginX: 15.w,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 10.w,
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadiusGeometry.circular(35.r),
+              child: Image.asset(
+                imagePath,
+                height: 100.h,
+                width: 130.w,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    title,
+                    style: TextStyle(
+                      color: AppColors.textColor.value,
+                      fontSize: FontScaler.fromSize(FontSize.xl),
+                    ),
+                  ),
+                  Text(
+                    content,
+                    style: TextStyle(
+                      color: AppColors.textColor.value,
+                      fontSize: FontScaler.fromSize(FontSize.lg),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
@@ -60,26 +65,26 @@ class AddReminderTypeScreen extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisAlignment: MainAxisAlignment.start,
-			spacing: 15.w,
+      spacing: 15.w,
       children: <Widget>[
-				_button(
-					'lib/assets/images/medication.jpg', 
-					'Medicación', 
-					'Agrega recordatorios para pastillas, ampollas, jarabes, etc.',
-					(){}
-				),
-				_button(
-					'lib/assets/images/activity.png', 
-					'Actividad Física', 
-					'Programa una actividad física que desees realizar.',
-					(){}
-				),
-				_button(
-					'lib/assets/images/appointment.png', 
-					'Cita Médica', 
-					'Programa una cita médica y ten un recordatorio para no olvidarla.',
-					(){}
-				)
+        _button(
+          'lib/assets/images/medication.jpg',
+          'Medicación',
+          'Agrega recordatorios para pastillas, ampollas, jarabes, etc.',
+          () {},
+        ),
+        _button(
+          'lib/assets/images/activity.png',
+          'Actividad Física',
+          'Programa una actividad física que desees realizar.',
+          () {},
+        ),
+        _button(
+          'lib/assets/images/appointment.png',
+          'Cita Médica',
+          'Programa una cita médica y ten un recordatorio para no olvidarla.',
+          () {},
+        ),
       ],
     );
   }
